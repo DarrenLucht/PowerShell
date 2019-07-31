a8063e8d9a4d90597db2b059c6e755556aa34de2
DarrenLucht <48996381+DarrenLucht@users.noreply.github.com>
2019-07-31 15:29:19 -0500

    Update Get-ExchangeConnectivity.ps1

--- Exchange/Get-ExchangeConnectivity.ps1
+++ Exchange/Get-ExchangeConnectivity.ps1
@@ -11,10 +11,7 @@ Use the Get-ExchangeConnectivity to view Exchange Server virtual directories and
 .NOTES
 Written by: Darren Lucht
 
-* Github:	https://github.com/DarrenLucht/PowerShell/tree/master/Exchange
-
-THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE 
-RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
+* Github:  https://github.com/DarrenLucht/PowerShell/tree/master/Exchange
 
 License:
 
